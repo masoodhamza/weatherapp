c872b51eb9518ad04feee08a0126c9abe2b42335
Hamza Masood - حمزہ مسعود <49762757+masoodhamza@users.noreply.github.com>
2021-04-22 14:37:58 +0500

    ui display complte (#3)

--- js/forcast.js
+++ js/forcast.js
@@ -1,29 +1,32 @@
 // api key
-apikey = 'bFBVy0gRO7A35aTIiX7rEDrYaudYGQC3';
+apikey = "bFBVy0gRO7A35aTIiX7rEDrYaudYGQC3";
 
 // get city
 const getCity = async (city) => {
-    const baseUrl = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const queryParameters = `?apikey=${apikey}&q=${city}`;
-    const response = await fetch(baseUrl + queryParameters);
-    const data = await response.json();    
-    return data;
-}
+  const baseUrl =
+    "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const queryParameters = `?apikey=${apikey}&q=${city}`;
+  const response = await fetch(baseUrl + queryParameters);
+  const data = await response.json();
+  return data[0];
+};
 
-getCity('lahore')
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+// getCity("new york")
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
 //get weather of the city
 const getWeather = async (locationKey) => {
-    const baseUrl = 'http://dataservice.accuweather.com/currentconditions/v1/';
-    const queryParameters = `${locationKey}?apikey=${apikey}`;
-    const response = await fetch(baseUrl + queryParameters);
-    const data = await response.json();
-    return data;
-}
+  const baseUrl = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const queryParameters = `${locationKey}?apikey=${apikey}`;
+  const response = await fetch(baseUrl + queryParameters);
+  const data = await response.json();
+  return data[0];
+};
 
-getCity('Lahore')
-    .then(data => {return getWeather(data[0].Key)})
-    .then(data => console.log(data[0]))
-    .catch(err => console.log(err))
+// getCity("new york")
+//   .then((data) => {
+//     return getWeather(data.Key);
+//   })
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
